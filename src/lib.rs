dcaa26b7bfe64f49a659ed34bbf8a072453859e7
Fabien <ftrestour@gmail.com>
2023-08-05 23:05:12 +0200

    ✨ Basic code structure

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+pub fn run_file(filename: &str) {
+    print!("Run file: {}", filename);
+    todo!()
+}
+
+pub fn run_prompt() {
+    print!("Run prompt");
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, process};
+
+use rlox::{run_file, run_prompt};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        run_file(&args[1]);
+    } else if args.len() == 1 {
+        run_prompt();
+    } else {
+        println!("Usage: rlox [script]");
+        process::exit(64);
+    }
 }
