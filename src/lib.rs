1080d5e358c4adb306bc50d8f47dc6e8b98e770d
Fabien <ftrestour@gmail.com>
2023-08-05 23:23:46 +0200

    ✨ Base run functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,25 @@
-pub fn run_file(filename: &str) {
-    print!("Run file: {}", filename);
-    todo!()
+use anyhow::Result;
+use std::{fs, io};
+
+pub fn run_file(filename: &str) -> Result<()> {
+    let file = fs::read_to_string(&filename)?;
+    run(&file)
 }
 
-pub fn run_prompt() {
-    print!("Run prompt");
+pub fn run_prompt() -> Result<()> {
+    loop {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line)?;
+
+        let line = line.trim();
+
+        if !line.is_empty() {
+            run(line)?;
+        }
+    }
+}
+
+fn run(code: &str) -> Result<()> {
+    print!("Running code: {}", code);
     todo!()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
+use anyhow::Result;
 use std::{env, process};
 
 use rlox::{run_file, run_prompt};
 
-fn main() {
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() == 2 {
-        run_file(&args[1]);
+        run_file(&args[1])
     } else if args.len() == 1 {
-        run_prompt();
+        run_prompt()
     } else {
         println!("Usage: rlox [script]");
         process::exit(64);
