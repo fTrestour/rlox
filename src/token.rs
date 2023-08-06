22afe1aad6fe6bbe312a306ec5cda4e13cc3202e
Fabien <ftrestour@gmail.com>
2023-08-06 07:53:23 +0200

    🏷️ Define tokens

--- src/token.rs
+++ src/token.rs
@@ -1,2 +1,59 @@
+use std::fmt;
+
+use crate::types::Line;
+
 #[derive(Debug)]
-pub enum Token {}
+pub enum TokenType {
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    Identifier(String),
+    String(String),
+    Number(String),
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    Eof,
+}
+
+#[derive(Debug)]
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub line: Line,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?} {} l.{}", self.token_type, self.lexeme, self.line)
+    }
+}
