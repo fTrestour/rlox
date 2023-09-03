0fe05652cc78602b10b93b9f46314712ae161cfb
Fabien <ftrestour@gmail.com>
2023-09-03 17:07:10 +0200

    👔 Clock gets more precise

--- src/standard.rs
+++ src/standard.rs
@@ -7,7 +7,7 @@ pub fn clock(_: Vec<Value>) -> Value {
     let since_the_epoch = start
         .duration_since(UNIX_EPOCH)
         .expect("Time went backwards")
-        .as_secs();
+        .as_millis();
 
-    Value::Number(since_the_epoch as f64)
+    Value::Number(since_the_epoch as f64 / 1000.)
 }
